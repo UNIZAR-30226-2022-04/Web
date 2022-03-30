c3e29ea2d75056baa9b4046055d7e9440edaafd6
777524jma <76255657+Chechu236@users.noreply.github.com>
2022-03-30 18:20:27 +0200

    Preparación interación BD Login

--- frankenstory-web/components/Login.js
+++ frankenstory-web/components/Login.js
@@ -1,18 +1,27 @@
-import styles from "./Login.module.css"
-
+import { useState } from "react"
 import Link from "next/Link"
 
 const Login = () => {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onSubmit = (e)=>{
+    e.preventDefault()
+    console.log(name)
+    console.log(password)
+  }
+
   return (
-    <div class= "p-6 h-screen w-screen bg-white flex items-center bg-blue-100">
-    <form class ="m-auto justify-center p-6 bg-white align-middle">
-        <h1 class="pb-3 pt-6 px-10 text-4xl text-blue-800 font-bold">Iniciar sesión</h1>
+    <div className= "p-6 h-screen w-screen bg-white flex items-center bg-blue-100">
+    <form className ="m-auto justify-center p-6 bg-white align-middle" onSubmit={onSubmit}>
+        <h1 className="pb-3 pt-6 px-10 text-4xl text-blue-800 font-bold">Iniciar sesión</h1>
         <div>Usuario</div>
-        <input class="p-2 w-full bg-blue-100 text-blue-400" type="text" placeholder="Usuario" /*onChange={(e) => setText(e.target.value)}*//>
+        <input className="p-2 w-full bg-blue-100 text-blue-400" type="text" placeholder="Usuario" onChange={(e) => setName(e.target.value)}/>
         <div>Contraseña</div>
-        <input class="p-2 w-full bg-blue-100 text-blue-400" type="text" placeholder="Usuario" /*onChange={(e) => setText(e.target.value)}*//>
-        <div className = {styles.link}><Link  href="/">Crear Cuenta</Link></div>
-        <input class="m-auto" type="submit" value="Iniciar sesión"/>
+        <input className="p-2 w-full bg-blue-100 text-blue-400" type="text" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>
+        <div className="py-2 font-bold text-blue-800"><Link  href="/">Crear Cuenta</Link></div>
+        <input className="ml-20 bg-center text-white justify-self-center bg-blue-900 rounded-md px-4 py-2"
+        type="submit" value="Iniciar sesión"/>
     </form>
     </div>
   )
