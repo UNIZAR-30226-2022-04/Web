2760e1704bea19a08e968de834db2f66202a93e0
Jorge Lisa <774248@unizar.es>
2022-04-01 23:27:33 +0200

    fix: added more circles to rulette and fixed others
    Tarea: hacer que dependan del tamaño de la pantalla

--- frankenstory-web/components/Rulette.js
+++ frankenstory-web/components/Rulette.js
@@ -3,7 +3,11 @@ export default function Rulette({data}) {
         <>
             <div class='rulette_outter'>
                 <div class='rulette_inner'>
-
+                    <div class='rulette_inner_2'>
+                        <div class='rulette_inner_3'>
+    
+                        </div>
+                    </div>
                 </div>
             </div>                  
         </>
