07f767f6e3f9b2592532852ceabb2a67ec70c0a8
777524jma <76255657+Chechu236@users.noreply.github.com>
2022-04-09 11:53:19 +0200

    a

--- frankenstory-web/components/Login.js
+++ frankenstory-web/components/Login.js
@@ -3,26 +3,39 @@ import Link from "next/link"
 
 const tryLogin = async (user,pass) => {
 
-  let response
-
-  //var url = "https://mooncode-frankenstory-dev.herokuapp.com/api/login"
-  let url ="https://localhost:3001/api/login"
-
-  var data = {
+  const data = {
     username:user,
     password:pass
   }
+
+  //const url ="https://localhost:3001/api/login"
+  const url = "https://mooncode-frankenstory-dev.herokuapp.com/api/login"
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json' 
+    },
+    body: JSON.stringify(data)
+  }
+  var res = await fetch(url,options)
+  var jsonRes = await res.json()
+
+  console.log(jsonRes)
   
-  response = await fetch(url,{
+ 
+
+  /*response = await fetch(url,{
     method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
     },
     body: JSON.stringify(data),
-  })
-  console.log(JSON.stringify(data))
-  response = await response.json();
-  console.log(response)
+  })*/  
+  //console.log(JSON.stringify(data))
+  //response = await response.json();
+  //console.log(response)
 }
 
 const Login = () => {
@@ -43,7 +56,7 @@ const Login = () => {
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
 
-  const goTo = '/' + name + '/friends'
+  //const goTo = '/' + name + '/friends'
 
   return (
       <form className ="m-auto justify-center p-6 bg-white align-middle" onSubmit={onSubmit}>
@@ -52,7 +65,7 @@ const Login = () => {
           <input className="p-2 w-full bg-blue-100 text-blue-400" type="text" value={name} placeholder="Usuario" onChange={(e) => setName(e.target.value)}/>
           <div>Contraseña</div>
           <input className="p-2 w-full bg-blue-100 text-blue-400" type="text" value={password} placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>
-          <div className="py-2 font-bold text-blue-800"><Link  href="/">Crear Cuenta</Link></div>
+          <div className="py-2 font-bold text-blue-800"><Link  href="/register">Crear Cuenta</Link></div>
           <button className="ml-20 bg-center text-white justify-self-center bg-blue-900 rounded-md px-4 py-2" type="submit">Iniciar sesión</button>
       </form>
   )
