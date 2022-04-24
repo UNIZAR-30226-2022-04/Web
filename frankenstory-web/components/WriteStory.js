7b0769c5c229cec212122c45673178a1a2bcdbef
777524jma <76255657+Chechu236@users.noreply.github.com>
2022-04-24 14:14:17 +0200

    Trabajo en modo relato

--- frankenstory-web/components/WriteStory.js
+++ frankenstory-web/components/WriteStory.js
@@ -1,35 +1,36 @@
 import { useState } from "react"
-import React from "react"
-
-const currentTitle = "Placeholder"
 
 const WriteStory = ({user,first}) => {
 
   const [currentTitle, setCurrentTitle] = useState("")
   const [currentText, setCurrentText] = useState("")
+  const title = "Titulaco"
+  const body = "Tremendo parrafaco de increible y profunda prosa"
 
   const onSubmit = (e) => {
     console.log("Hey")
   }
 
- 
-
-
   return (
       <div className="storyBox">
-        {first ? <div className="commonTitle text-center bg-red-200 h-10">Empieza tu relato </div> : <h1>{currentTitle}</h1>}
-        <form className="bg-blue-200" onSubmit={onSubmit}>
-            <div>
-              {first ? <input className="titleWrite mx-20 w-full ml-0 bg-green-600" type="text" maxLength={200} value={currentTitle} placeholder="Titulo del relato" onChange={(e) => setCurrentTitle(e.target.value)}/>
-              :"" }
+        <form onSubmit={onSubmit}>
+            <div className="centered">
+              <div className="commonTitle h-auto">{first ? <>Empieza tu relato</> : <>{title}</> } </div>
             </div>
-            <div className="text-center">
-              <input className="storyWrite ml-10 h-auto w-10/12" type="text" maxLength={200} value={currentText} placeholder="Escribe tu parrafo" onChange={(e) => setCurrentText(e.target.value)}/>
+            <div className="centered">
+              {first ? <input className="titleWrite w-6/12 mx-0 bg-gray-200" type="text" maxLength={200} value={currentTitle} placeholder="Titulo del relato" onChange={(e) => setCurrentTitle(e.target.value)}/>
+              :<div className="mb-10">{body}</div> }
             </div>
-            <div className="text-center">
-              <input className="bg-red-800" type="submit" value="Enviar parrafo"/>
+            <div className="centered">
+              <input className="storyWrite h-auto w-8/12" type="text" maxLength={200} value={currentText} placeholder="Escribe tu parrafo" onChange={(e) => setCurrentText(e.target.value)}/>
             </div>
-            { !first ? <div><input type="button" value = "Terminar relato"/></div>
+            <div className="clickable-item centered">
+              <input className="bg-blue-400 w-32 rounded-xl" type="submit" value="Enviar parrafo"/>
+            </div>
+            { !first ? 
+            <div className="clickable-item centered">
+              <input className ="bg-red-400 w-32 rounded-xl" type="submit" value = "Terminar relato"/>
+            </div>
             : ""}
         </form>
       </div>
