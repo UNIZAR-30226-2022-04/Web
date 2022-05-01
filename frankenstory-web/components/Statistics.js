4eccadcf7b9f57ca59689c3ab420331057b5513d
Jorge Lisa <774248@unizar.es>
2022-05-01 20:32:22 +0200

    feat: Storymode lobby advance

--- frankenstory-web/components/Statistics.js
+++ frankenstory-web/components/Statistics.js
@@ -1,38 +1,15 @@
 import Image from 'next/image'
 
-let best = true
+import ListOfPeople from './ListOfPeople'
 
 export default function Stats({data}) {
     return(
-        <div className='relative pl-20 h-screen flex flex-col justify-center space-y-2'>
+        <div className='pl-20 flex flex-col justify-center space-y-2 absolute left-0'>
             <div className='relative flex flex-row h-auto w-auto'>
                 <Image src='/icons/stats.png' width="40" height="25" />
                 <h1 className='franken'>Statistics</h1>
-            </div>
-            
-            <div className='flex flex-col space-y-4'>
-                {data.map((person, index) => ( <PersonInfo key={index} person={person} index={index} /> ) ) }
             </div>  
+            <ListOfPeople data={data} />
         </div>
-    )
-
-}
-
-function PersonInfo({person, index}){
-    return( 
-        <div className='flex flex-row items-center bg-teal-200 p-1 rounded-lg'>
-            
-            <h1 className='w-auto xl:text-2xl text-sm text-emerald-600 font-bold'>{index+1}º</h1>
-            {index == 0 ? (
-                <div className='xl:w-20 w-10'>
-                    <Image src='/icons/crown.png' width="38" height="38"/>
-                </div>                
-            ):(
-                <div className='xl:w-20 w-10'/>
-            )}
-            <h1 className='xl:w-32 w-10 xl:text-2xl text-sm text-emerald-600 font-bold clickableItem'>{person.username}</h1>
-            <h1 className='pl-24 xl:text-2xl text-sm text-emerald-600 font-bold'>{person.stars}</h1>
-            <Image src='/icons/star.png' width="38" height="38" />
-        </div>  
     )
 }
--- frankenstory-web/pages/quickGame/lobby/index.js
+++ frankenstory-web/pages/quickGame/lobby/index.js
@@ -1,8 +1,11 @@
-import Layout from 'components/Layout'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
-const user = {"username": "MrNOPatineto", "password": "12345"}
-const creator = "MrNOPatineto"
+import Layout from 'components/Layout'
+import Spinner from 'components/Spinner'
+import ListOfPeople from 'components/ListOfPeople'
+
 const sala = {
     result:"success",
     mode: "random", // "random | twitter"
@@ -23,85 +26,125 @@ const sala = {
   }
   
 
-export default function QuickGame({userInfo}){
-    const layoutInfo = {
-        username: user.username,
-        stars:    userInfo.stars,
-        coins:    userInfo.coins,
-        image_ID: userInfo.picture
+export default function QuickGame(){
+    const router = useRouter()
+    const [myuser, setMyuser] = useState("")  // Hook que devuelve la llamada de la api
+    const [windowUser, setWindowUser] = useState({})
+    const [code, setCode] = useState("")
+
+    useEffect(()=>{
+        if(localStorage.getItem("logged") == "si"){
+            const username = localStorage.getItem("username")
+            const password = localStorage.getItem("password")
+            setWindowUser({username: username, password: password})
+            console.log("SACO DATOS")
+        }else{
+            console.log("VOY A LOGIN")
+            router.push("/")
+        }
+        const queryParams = new URLSearchParams(window.location.search);
+        setCode(queryParams.get('code'))
+    }, [])
+        
+    // Hace fetch de la api
+    useEffect(() => {
+        // Función que llama a la api
+        if(!windowUser.username || !code){
+            console.log("no permito sacar datos")
+            return
+        }
+            
+        const getData = async () => {
+            // Opciones para llamar a la api
+            const options = {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(windowUser) 
+            }
+            
+            const info = {
+                username: windowUser.username,
+                password: windowUser.password,
+                id: code
+            }
+
+            const options2 = {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(info) 
+            }
+
+            // Llamada a la api
+            const res1 = await fetch('http://localhost:3000/api/general/home', options)
+            const data1 = await res1.json()
+            const res2 = await fetch('http://localhost:3000/api/quick_game/get_room', options)
+            const data2 = await res2.json()
+
+            //DEBUG
+            data2.result = "acierto"
+
+            // Si no ha ido bien o no estoy logeado volvemos a /
+            if(data1.result === "error" || data2.result === 'error'){
+                router.push("/")
+                return
+            }
+
+            // Llama al hook que almacena la información del usuario
+            setMyuser(data1)
+        }
+        getData()
+    }, [windowUser])
+
+    // Si tadavía no hoy usuario, esperamos a que lo haya
+    if(!myuser){
+        return <Spinner />
     }
-    
+
+    // Renderizamos la página
+    const layoutInfo = {
+        username: windowUser.username,
+        stars:    myuser.stars,
+        coins:    myuser.coins,
+        image_ID: myuser.picture
+    } 
+
     return(
         <Layout data={layoutInfo} >
-            <div className='flex flex-row w-screen items-center h-screen space-x-20 ml-5'>
-                <ListOfPlayers participants={sala.participants} />
-                <div className='flex flex-col bg-white'>
-                    <div className='flex flex-row'>
-                        <p>Tiempo: </p>
-                        <Image src='/quick-game/clock.png' width="30" height="30"/>
+            <div className='flex flex-col items-center justify-center w-screen space-y-5'>
+                <div className='commonTitle'>Código de la Sala: {code}</div>
+                <div className='flex flex-row justify-center items-center space-x-20 ml-5'>
+                    <ListOfPeople data={sala.participants} />
+                    <div className='flex flex-col  space-y-5'>
+                        <div className='flex flex-row bg-white rounded-lg items-center justify-center p-2 space-x-2'>
+                            <Image src='/quick-game/clock.png' width="30" height="30"/>
+                            <p className=''>Tiempo: </p>                            
+                        </div>
+                        
+                        {sala.mode == 'random'?(
+                            <div className='flex flex-row bg-purple-500 rounded-lg items-center justify-center space-x-2 p-2'>
+                                <Image src='/quick-game/random_words.png' width="30" height="30"/>
+                                <p className=''>Modo Aleatorio</p>                                
+                            </div>
+                        ):(
+                            <div className='flex flex-row bg-blue-500'>
+                                <p>Modo Tendencias Twitter</p>
+                                <Image src='/quick-game/twitter_trend.png' width="30" height="30"/>
+                            </div>
+                        )}             
                     </div>
-                    
-                    {sala.mode == 'random'?(
-                        <div className='flex flex-row bg-purple-500'>
-                            <p>Modo Aleatorio</p>
-                            <Image src='/quick-game/random_words.png' width="30" height="30"/>
-                        </div>
-                    ):(
-                        <div className='flex flex-row bg-blue-500'>
-                            <p>Modo Tendencias Twitter</p>
-                            <Image src='/quick-game/twitter_trend.png' width="30" height="30"/>
-                        </div>
-                    )}             
                 </div>
-            </div>            
+                <button className='buttonStyle bg-purple-500' onClick={() => leaveRoom(windowUser, router)}> {'<-'} VOLVER</button>  
+            </div>                      
         </Layout>
     )
 }
 
-function ListOfPlayers({participants}){
-    
-    return(
-        <>
-            <div className='flex flex-col space-y-2'>
-                <h1>Paricipantes {participants.length}/10</h1>
-                {participants.map( (player) => ( <Player key={player.toString()} info={player} /> ) )}
-                <div className='flex flex-col'>
-                    <h1></h1>
-                </div>
-            </div>
-        </>        
-    )
-}
+function leaveRoom(user, router){
+    router.push("/quickGame")
 
-function Player({info}){
-    console.log(info)
-    const imageRoute = '/profPic/icon' + info.picture + '.png'
-    return(
-        <div className='flex flex-row px-2 b-white'>
-            <div className='w-14'><Image src={imageRoute} width="30" height="30"/></div>
-            <p className='w-32'>{info.username}</p>
-            <p className='w-14'>x{info.stars}</p>
-            <div><Image src='/icons/star.png' width="30" height="30"/></div>
-        </div>
-    )
-}
-
-export async function getStaticProps () {
-    
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user) 
-    }
-    
-    const friendStats = await fetch('http://localhost:3000/api/general/home', options)
-    const userInfo = await friendStats.json()
-  
-    return {
-      props: {
-            userInfo,
-        }
-    }
+    // HACER FECT DE LEAVE ROOM
 }
