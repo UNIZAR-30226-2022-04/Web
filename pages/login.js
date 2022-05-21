a81f5a09ea639dfad1485fa3bbdcbc552b710edd
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-05-21 17:17:00 +0200

    Heroku push

--- pages/login.js
+++ pages/login.js
@@ -1,143 +1,157 @@
-import { useState, useEffect } from "react"
-import {useRouter} from 'next/router'
-import Link from "next/link"
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
 
-import Lottie from 'react-lottie'
-import loginLottie from '/public/lottie/login.json'
+//import Lottie from 'react-lottie'
+//import loginLottie from '/public/lottie/login.json'
 
-var sha512 = require ("sha512")
+var sha512 = require("sha512");
 
 export default function Login() {
-  const router = useRouter()  
-  const [name, setName] = useState("")
-  const [password, setPassword] = useState("")
+	const router = useRouter();
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
 
-  // Check de que el usuario no esté ya logueado
-  useEffect(()=>{
-    if(localStorage.getItem("logged") == "si"){
-      router.push("/profile")
-    }
-  }, [router])
+	// Check de que el usuario no esté ya logueado
+	useEffect(() => {
+		if (localStorage.getItem("logged") == "si") {
+			router.push("/profile");
+		}
+	}, [router]);
 
-  const options = {
-    loop: true,
-    autoplay: true,
-    animationData: loginLottie,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  }
-
-  return (
-    <div className="background h-screen w-screen flex flex-col items-center justify-center">
-      <Image src={'/frankenstory.png'} height={150} width={600} alt={"LOGO"} />
-      <div className="flex flex-row justify-center items-center">
-        <Lottie 
-          options = {options}
-          height = {400}
-          width = {400}
-        />
-        <form className ="flex flex-col items-center justify-center" onSubmit={(e) => (onSubmit(e, name, password, router))}>
-            <h1 className="commonTitle font-arial-b">INICIAR SESIÓN</h1>
-            <div className="w-96 flex flex-col space-y-2">
-              <div>
-                <div className="commonSubtitle">Usuario</div>
-                <input className="w-full p-2 bg-white rounded-lg" maxLength={30} type="text" value={name} placeholder="Usuario" onChange={(e) => setName(e.target.value)}/>
-              </div>
-              <div>
-                <div className="commonSubtitle">Contraseña</div>
-                <input className="w-full p-2 bg-white rounded-lg" maxLength={30} type="password" value={password} placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>
-              </div>
-              <button className="commonButton bg-verde_top" type="submit">Iniciar sesión</button>
-            </div>
-            
-        </form>
-      </div>
-      <button className="commonButton bg-verde_top absolute bottom-0 right-0 m-10" onClick={() => (router.push("/register"))}>Crear Cuenta {"->"}</button>
-    </div>
-  )
+	return (
+		<div className="background h-screen w-screen flex flex-col items-center justify-center">
+			<Image
+				src={"/frankenstory.png"}
+				height={150}
+				width={600}
+				alt={"LOGO"}
+			/>
+			<div className="flex flex-row justify-center items-center">
+				<form
+					className="flex flex-col items-center justify-center"
+					onSubmit={(e) => onSubmit(e, name, password, router)}
+				>
+					<h1 className="commonTitle font-arial-b">INICIAR SESIÓN</h1>
+					<div className="w-96 flex flex-col space-y-2">
+						<div>
+							<div className="commonSubtitle">Usuario</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								maxLength={30}
+								type="text"
+								value={name}
+								placeholder="Usuario"
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div>
+							<div className="commonSubtitle">Contraseña</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								maxLength={30}
+								type="password"
+								value={password}
+								placeholder="Contraseña"
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<button
+							className="commonButton bg-verde_top"
+							type="submit"
+						>
+							Iniciar sesión
+						</button>
+					</div>
+				</form>
+			</div>
+			<button
+				className="commonButton bg-verde_top absolute bottom-0 right-0 m-10"
+				onClick={() => router.push("/register")}
+			>
+				Crear Cuenta {"->"}
+			</button>
+		</div>
+	);
 }
-
 
 const onSubmit = (e, name, password, router) => {
-  var salt
-  var hash
-  e.preventDefault()
-  if(name == ""){
-    alert("Introduce un nombre de usuario")
-  
-  }else if(password == ""){
-    alert("Introduce una contraseña")
-  
-  }else{
-    getSalt(name).then((res) => {
-      if(res.result == "success"){
-        salt = res.salt
-        hash = sha512(password+salt)
-        hash = hash.toString("hex")
-        
-        tryLogin(name, hash).then((res2) =>{
-          if(res2.result == "success"){
-            localStorage.setItem("logged", "si")
-            localStorage.setItem("username", name)
-            localStorage.setItem("password", hash)
-            
-            router.push("http://localhost:3000/profile")
-          
-          }else{
-            if(res2.reason == "user_not_found"){
-              alert("Usuario desconocido")
-            }else if(res2.reason == "wrong_password"){
-              alert("Contraseña incorrecta")
-            }else{
-              alert("Error desconocido")
-            }
-          }
-        })
-      }else{
-        alert("Error contraseña segura")
-        return
-      }
-    })
-  }
-}
+	var salt;
+	var hash;
+	e.preventDefault();
+	if (name == "") {
+		alert("Introduce un nombre de usuario");
+	} else if (password == "") {
+		alert("Introduce una contraseña");
+	} else {
+		getSalt(name).then((res) => {
+			if (res.result == "success") {
+				salt = res.salt;
+				hash = sha512(password + salt);
+				hash = hash.toString("hex");
 
-const tryLogin = async (user,pass) => {
+				tryLogin(name, hash).then((res2) => {
+					if (res2.result == "success") {
+						localStorage.setItem("logged", "si");
+						localStorage.setItem("username", name);
+						localStorage.setItem("password", hash);
 
-  const info = {
-    username:user,
-    password:pass
-  }
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json' 
-    },
-    body: JSON.stringify(info)
-  }
+						router.push("http://localhost:3000/profile");
+					} else {
+						if (res2.reason == "user_not_found") {
+							alert("Usuario desconocido");
+						} else if (res2.reason == "wrong_password") {
+							alert("Contraseña incorrecta");
+						} else {
+							alert("Error desconocido");
+						}
+					}
+				});
+			} else {
+				alert("Error contraseña segura");
+				return;
+			}
+		});
+	}
+};
 
-  const res = await fetch("http://localhost:3000/api/general/login", options)
-  const data = await res.json()
-  return data
-}
+const tryLogin = async (user, pass) => {
+	const info = {
+		username: user,
+		password: pass,
+	};
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(info),
+	};
 
-const getSalt = async (name) =>{
+	const res = await fetch("http://localhost:3000/api/general/login", options);
+	const data = await res.json();
+	return data;
+};
 
-  const info = {
-    username:name
-  }
+const getSalt = async (name) => {
+	const info = {
+		username: name,
+	};
 
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json' 
-    },
-    body: JSON.stringify(info)
-  }
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(info),
+	};
 
-  const res = await fetch("http://localhost:3000/api/general/get_salt", options)
-  return res.json()
-}
+	const res = await fetch(
+		"http://localhost:3000/api/general/get_salt",
+		options
+	);
+	return res.json();
+};
--- pages/register.js
+++ pages/register.js
@@ -1,123 +1,143 @@
-import { useState, useEffect } from "react"
-import {useRouter} from 'next/router'
-import Link from "next/link"
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
 
-import Lottie from 'react-lottie'
-import registerLottie from '/public/lottie/register.json'
+//import Lottie from 'react-lottie'
+//import registerLottie from '/public/lottie/register.json'
 
-var sha512 = require(`sha512`)
-var SecureRandom = require('securerandom');
+var sha512 = require(`sha512`);
+var SecureRandom = require("securerandom");
 
-export default function Register () {
-  const [name, setName] = useState("")
-  const [mail, setMail] = useState("")
-  const [password, setPassword] = useState("")
-  const [passwordR, setPasswordR] = useState("")
+export default function Register() {
+	const [name, setName] = useState("");
+	const [mail, setMail] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordR, setPasswordR] = useState("");
 
-  const router = useRouter()
+	const router = useRouter();
 
-  const options = {
-    loop: true,
-    autoplay: true,
-    animationData: registerLottie,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  }
-
-  return (
-    <div className="background h-screen w-screen flex flex-col items-center justify-center">
-      <Image src={'/frankenstory.png'} height={150} width={600} />
-      <div className="flex flex-row items-center justify-center">
-        <Lottie 
-            options = {options}
-            height = {400}
-            width = {400}
-          />
-        <form className ="flex flex-col items-center justify-center" onSubmit={(e) => (onSubmit(e, name, mail, password, passwordR, router))}>
-            <h1 className="commonTitle font-arial-b">REGISTER</h1>
-            <div className="flex flex-col w-96 space-y-2">
-              <div>
-                <div className="commonSubtitle">Nombre de usuario</div>
-                <input className="w-full p-2 bg-white rounded-lg" type="text" value={name} placeholder="Nombre de usuario" onChange={(e) => setName(e.target.value)}/>
-              </div>
-              <div>
-                <div className="commonSubtitle">Email</div>
-                <input className="w-full p-2 bg-white rounded-lg" type="email" value={mail} placeholder="Email" onChange={(e) => setMail(e.target.value)}/>
-              </div>
-              <div>
-                <div className="commonSubtitle">Contraseña</div>
-                <input className="w-full p-2 bg-white rounded-lg" type="password" value={password} placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>    
-              </div>
-              <div>
-                <div className="commonSubtitle">Repita la contraseña</div>
-                <input className="w-full p-2 bg-white rounded-lg" type="password" value={passwordR} placeholder="Repita la contraseña" onChange={(e) => setPasswordR(e.target.value)}/>
-              </div>
-              <button className="commonButton bg-verde_top mt-2" type="submit">Crear cuenta</button>
-            </div>
-        </form>
-      </div>
-      <button className="absolute bottom-0 left-0 m-10 commonButton bg-verde_top " onClick={() => (router.push("/login"))} > {'<-'} Iniciar Sesión</button>
-    </div>
-
-  )
+	return (
+		<div className="background h-screen w-screen flex flex-col items-center justify-center">
+			<Image src={"/frankenstory.png"} height={150} width={600} />
+			<div className="flex flex-row items-center justify-center">
+				<form
+					className="flex flex-col items-center justify-center"
+					onSubmit={(e) =>
+						onSubmit(e, name, mail, password, passwordR, router)
+					}
+				>
+					<h1 className="commonTitle font-arial-b">REGISTER</h1>
+					<div className="flex flex-col w-96 space-y-2">
+						<div>
+							<div className="commonSubtitle">
+								Nombre de usuario
+							</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								type="text"
+								value={name}
+								placeholder="Nombre de usuario"
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div>
+							<div className="commonSubtitle">Email</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								type="email"
+								value={mail}
+								placeholder="Email"
+								onChange={(e) => setMail(e.target.value)}
+							/>
+						</div>
+						<div>
+							<div className="commonSubtitle">Contraseña</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								type="password"
+								value={password}
+								placeholder="Contraseña"
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<div>
+							<div className="commonSubtitle">
+								Repita la contraseña
+							</div>
+							<input
+								className="w-full p-2 bg-white rounded-lg"
+								type="password"
+								value={passwordR}
+								placeholder="Repita la contraseña"
+								onChange={(e) => setPasswordR(e.target.value)}
+							/>
+						</div>
+						<button
+							className="commonButton bg-verde_top mt-2"
+							type="submit"
+						>
+							Crear cuenta
+						</button>
+					</div>
+				</form>
+			</div>
+			<button
+				className="absolute bottom-0 left-0 m-10 commonButton bg-verde_top "
+				onClick={() => router.push("/login")}
+			>
+				{" "}
+				{"<-"} Iniciar Sesión
+			</button>
+		</div>
+	);
 }
 
 const tryRegister = async (user, pass, mail) => {
+	var salt = SecureRandom.hex(16);
 
-  var salt = SecureRandom.hex(16)
+	const info = {
+		username: user,
+		password: sha512(pass + salt).toString("hex"),
+		email: mail,
+		salt: salt,
+	};
 
-  const info = {
-    username: user,
-    password: (sha512(pass+salt)).toString("hex"),
-    email: mail,
-    salt: salt
-  }
+	const url = "http://localhost:3000/api/general/register";
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(info),
+	};
+	const res = await fetch(url, options);
+	return res.json();
+};
 
-  const url ="http://localhost:3000/api/general/register"
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json' 
-    },
-    body: JSON.stringify(info)
-  }
-  const res = await fetch(url,options)
-  return res.json()
-}
-
-async function onSubmit (e, name, mail, password, passwordR, router) {
-    
-  e.preventDefault()
-  if(name == ""){
-    alert("Introduce un nombre de usuario")
-  
-  }else if(mail == ""){
-    alert("Introduce un correo electronico")
-  
-  }else if(password == ""){
-    alert("Introduce una contraseña")
-  
-  }else if(password != passwordR){
-    alert("Las contraseñas introducidas no coinciden")
- 
-  }else{
-    const res = await tryRegister(name, password, mail)
-    if(res.result == "success"){
-      window.location = `http://localhost:3000/profile`
-    }else{
-      if(res.reason == "user_already_registered"){
-        alert("El nombre de usuario introducido ya esta en uso")
-
-      }else if(res.reason == "email_already_registered"){      
-        alert("El correo introducido ya esta en uso")
-      
-      }else{
-        alert("Error desconocido")
-      }
-    }
-    
-  }
+async function onSubmit(e, name, mail, password, passwordR, router) {
+	e.preventDefault();
+	if (name == "") {
+		alert("Introduce un nombre de usuario");
+	} else if (mail == "") {
+		alert("Introduce un correo electronico");
+	} else if (password == "") {
+		alert("Introduce una contraseña");
+	} else if (password != passwordR) {
+		alert("Las contraseñas introducidas no coinciden");
+	} else {
+		const res = await tryRegister(name, password, mail);
+		if (res.result == "success") {
+			window.location = `http://localhost:3000/profile`;
+		} else {
+			if (res.reason == "user_already_registered") {
+				alert("El nombre de usuario introducido ya esta en uso");
+			} else if (res.reason == "email_already_registered") {
+				alert("El correo introducido ya esta en uso");
+			} else {
+				alert("Error desconocido");
+			}
+		}
+	}
 }
